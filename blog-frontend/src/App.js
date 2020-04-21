a537c98c72d8c99e03e23301dd4ddb16dc1614da
overaction <kmc04@naver.com>
2020-04-21 14:23:15 +0000

    라우터 적용

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import {Route} from 'react-router-dom';
+import PostListPage from './pages/PostListPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
+import PostPage from './pages/PostPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <Fragment>
+            <Route component={PostListPage} path={['/@:username', '/']} exact />
+            <Route component={LoginPage} path="/login" />
+            <Route component={RegisterPage} path="/register" />
+            <Route component={WritePage} path="/write" />
+            <Route component={PostPage} path="/@:username/:postId" />
+        </Fragment>
+    )
 }
 
 export default App;
+
+// PostListPage 처럼 path에 배열을 넣어주면 여러개의 경로를 한번에 설정할 수 있다
+// /@:username 처럼 @을 사용하면 http://localhost:3000/@kmc 에서처럼 kmc를 username 파라미터로 읽을 수 있다
